4d940ad43e177d9c9a3107609a0d76cf4a501e3b
Jens Rehsack <sno@netbsd.org>
2013-12-04 17:34:13 +0100

    add string overload for output generator

--- lib/App/Math/Trainer/Role/Unit.pm
+++ lib/App/Math/Trainer/Role/Unit.pm
@@ -18,10 +18,20 @@ use Hash::MoreUtils qw/slice_def/;
 use List::MoreUtils qw/firstidx/;
 
 {
-    package #
-	Unit;
+    package    #
+      Unit;
 
     use Moo;
+    use overload '""' => sub { join( " ", @{ $_[0]->parts } ) };
+
+    has type => (
+                  is       => "ro",
+                  required => 1
+                );
+    has parts => (
+                   is        => "ro",
+                   rerquired => 1
+                 );
 }
 
 has unit_definitions => ( is => "lazy" );
@@ -126,21 +136,21 @@ sub _build_ordered_units_flatten_helper
 
     foreach my $upnm ( keys %{$unit_part} )
     {
-	my ( $min, $max, $factor ) = @{ $unit_part->{$upnm} }{qw(min max factor)};
-	defined $min or $min = 0;
-	defined $factor or $factor = 1;
-	my %upv = slice_def {
-			      min    => $min,
-			      max    => $max,
-			      factor => $factor,
-			      unit   => $upnm
-			    };
-	my @match;
-	$min and push( @match, "_le($min,\$_[0])" );
-	$max and push( @match, "_ge($max,\$_[0])" );
-	@match or @match = ("1");
-	$upv{match} = eval sprintf( "sub { %s };", join( " and ", @match ) );
-	push @flatten, \%upv;
+        my ( $min, $max, $factor ) = @{ $unit_part->{$upnm} }{qw(min max factor)};
+        defined $min    or $min    = 0;
+        defined $factor or $factor = 1;
+        my %upv = slice_def {
+                              min    => $min,
+                              max    => $max,
+                              factor => $factor,
+                              unit   => $upnm
+                            };
+        my @match;
+        $min and push( @match, "_le($min,\$_[0])" );
+        $max and push( @match, "_ge($max,\$_[0])" );
+        @match or @match = ("1");
+        $upv{match} = eval sprintf( "sub { %s };", join( " and ", @match ) );
+        push @flatten, \%upv;
     }
 
     @flatten;
@@ -154,18 +164,18 @@ sub _build_ordered_units
 
     foreach my $cat ( keys %$ud )
     {
-        my @base = _build_ordered_units_flatten_helper($ud->{$cat}->{base});
-        my @mult = _build_ordered_units_flatten_helper($ud->{$cat}->{multiplier});
-        my @div = _build_ordered_units_flatten_helper($ud->{$cat}->{divider});
+        my @base = _build_ordered_units_flatten_helper( $ud->{$cat}->{base} );
+        my @mult = _build_ordered_units_flatten_helper( $ud->{$cat}->{multiplier} );
+        my @div  = _build_ordered_units_flatten_helper( $ud->{$cat}->{divider} );
         my %ru;    # reworked unit
 
-	1 != scalar @base and die "Invalid unit description: $cat";
+        1 != scalar @base and die "Invalid unit description: $cat";
 
         @mult = sort { $b->{factor} <=> $a->{factor} } @mult;
         @div  = sort { $a->{factor} <=> $b->{factor} } @div;
-        $ru{base} = scalar @mult;
+        $ru{base}     = scalar @mult;
         $ru{spectrum} = [ @mult, @base, @div ];
-        $ou{$cat} = \%ru;
+        $ou{$cat}     = \%ru;
     }
 
     return \%ou;
@@ -176,21 +186,24 @@ sub _guess_unit_number
     my ( $unit_type, $lb, $ub ) = @_;
     my @rc;
 
-    $lb == $ub and $lb ==  scalar @{$unit_type->{spectrum}} and --$lb;
-    $lb == $ub and $ub == 0 and scalar @{$unit_type->{spectrum}} > 0 and ++$ub;
+    $lb == $ub and $lb == scalar @{ $unit_type->{spectrum} } and --$lb;
+    $lb == $ub and $ub == 0 and scalar @{ $unit_type->{spectrum} } > 0 and ++$ub;
     $lb == $ub and $ub < $unit_type->{base} and ++$ub;
     $lb == $ub and --$lb;
 
     my $i;
-    for($i = $lb; $i <= $ub; ++$i)
+    for ( $i = $lb; $i <= $ub; ++$i )
     {
-	my $max = defined $unit_type->{spectrum}->[$i]->{max} ? $unit_type->{spectrum}->[$i]->{max} : 100;
-	my $min = $unit_type->{spectrum}->[$i]->{min};
-	my $value = int(rand($max+$min)) - $min;
-	push(@rc, $value, $unit_type->{spectrum}->[$i]->{unit});
+        my $max =
+          defined $unit_type->{spectrum}->[$i]->{max} ? $unit_type->{spectrum}->[$i]->{max} : 100;
+        my $min   = $unit_type->{spectrum}->[$i]->{min};
+        my $value = int( rand( $max + $min ) ) - $min;
+        push( @rc, $value, $unit_type->{spectrum}->[$i]->{unit} );
     }
 
-    return Unit->new(type => $unit_type, parts => \@rc);
+    return
+      Unit->new( type  => $unit_type,
+                 parts => \@rc );
 }
 
 sub get_unit_numbers
@@ -212,7 +225,7 @@ sub get_unit_numbers
           $bounds[0] <= $bounds[1]
           ? _guess_unit_number( $ut, @bounds )
           : _guess_unit_number( $ut, $bounds[1], $bounds[0] );
-	push(@result, $unit);
+        push( @result, $unit );
     }
 
     return @result;
