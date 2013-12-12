01d78d6e58c03dd898a5d62bac72c77a74b24822
Jens Rehsack <sno@netbsd.org>
2013-12-12 16:43:47 +0100

    refactor vulgar fraction exercise generator
    Refactor vulgar fraction exercise generator at use-case addition/subtraction
    of vulgar fraction.

--- lib/App/Math/Trainer/Cmd/VulFrac/Cmd/Add.pm
+++ lib/App/Math/Trainer/Cmd/VulFrac/Cmd/Add.pm
@@ -29,56 +29,21 @@ sub _build_command_names
     return qw(add sub);
 }
 
-sub _euklid
-{
-    my ( $a, $b ) = @_;
-    my $h;
-    while ( $b != 0 ) { $h = $a % $b; $a = $b; $b = $h; }
-    return $a;
-}
-
-sub _gcd
-{
-    my ( $a, $b ) = @_;
-    my $gcd = $a > $b ? _euklid( $a, $b ) : _euklid( $b, $a );
-    return $gcd;
-}
-
-sub _reduce
-{
-    my ( $a, $b ) = @_;
-    my $gcd = $a > $b ? _euklid( $a, $b ) : _euklid( $b, $a );
-    $a /= $gcd;
-    $b /= $gcd;
-    return ( $a, $b );
-}
-
 sub _build_exercises
 {
     my ($self) = @_;
 
     my (@tasks);
-    my ( $maxa, $maxb ) = @{ $self->format };
 
     foreach my $i ( 1 .. $self->amount )
     {
-        my @numbers;
-
-        foreach my $i ( 0 .. 3 )
+        my @line;
+        foreach my $j ( 0 .. 1 )
         {
-            my ( $a, $b );
-            do
-            {
-                ( $a, $b ) = ( int( rand($maxa) ), int( rand($maxb) ) );
-            } while ( $a < 2 || $b < 2 );
-
-            $numbers[$i] = {
-                             num   => $a,
-                             denum => $b
-                           };
+            my ( $a, $b ) = $self->get_vulgar_fractions(2);
+            push @line, [ $a, $b ];
         }
-
-        push( @tasks, \@numbers );
+        push @tasks, \@line;
     }
 
     my $exercises = {
@@ -95,91 +60,71 @@ sub _build_exercises
     foreach my $line (@tasks)
     {
         my ( @solution, @challenge );
-        if ( ( $line->[2]->{num} / $line->[2]->{denum} ) <
-             ( $line->[3]->{num} / $line->[3]->{denum} ) )
-        {
-            # swap
-            my $tmp = $line->[2];
-            $line->[2] = $line->[3];
-            $line->[3] = $tmp;
-        }
 
         foreach my $i ( 0 .. 1 )
         {
-            my ( $a, $b ) = @$line[ 2 * $i, 2 * $i + 1 ];
+            my ( $a, $b ) = @{ $line->[$i] };
             my $op = $i ? '-' : '+';
-            push(
-                  @challenge,
-                  sprintf(
-                           '$ \frac{%d}{%d} %s \frac{%d}{%d} = $',
-                           $a->{num}, $a->{denum}, $op, $b->{num}, $b->{denum}
-                         )
-                );
+            $op eq '-' and $a < $b and ( $b, $a ) = ( $a, $b );
+            push( @challenge, sprintf( '$ %s %s %s = $', $a, $op, $b ) );
 
             my @way;    # remember Frank Sinatra :)
-            push(
-                  @way,
-                  sprintf(
-                           '\frac{%d}{%d} %s \frac{%d}{%d}',
-                           $a->{num}, $a->{denum}, $op, $b->{num}, $b->{denum}
-                         )
-                );
+            push( @way, sprintf( '%s %s %s', $a, $op, $b ) );
 
-            @$a{ 'num', 'denum' } = _reduce( @$a{ 'num', 'denum' } );
-            @$b{ 'num', 'denum' } = _reduce( @$b{ 'num', 'denum' } );
+            $a = $a->_reduce;
+            $b = $b->_reduce;
 
-            push(
-                  @way,
-                  sprintf(
-                           '\frac{%d}{%d} %s \frac{%d}{%d}',
-                           $a->{num}, $a->{denum}, $op, $b->{num}, $b->{denum}
-                         )
-                );
-            my $gcd = _gcd( $a->{denum}, $b->{denum} );
+            push( @way, sprintf( '%s %s %s', $a, $op, $b ) );
+            my $gcd = VulFrac->new(
+                                    num   => $a->denum,
+                                    denum => $b->denum
+                                  )->_gcd;
             my ( $fa, $fb ) = ( $b->{denum} / $gcd, $a->{denum} / $gcd );
 
             push(
                   @way,
                   sprintf(
                            '\frac{%d \cdot %d}{%d \cdot %d} %s \frac{%d \cdot %d}{%d \cdot %d}',
-                           $a->{num}, $fa, $a->{denum}, $fa, $op,
-                           $b->{num}, $fb, $b->{denum}, $fb
+                           $a->num, $fa, $a->denum, $fa, $op, $b->num, $fb, $b->denum, $fb
                          )
                 );
             push(
                   @way,
                   sprintf(
                            '\frac{%d}{%d} %s \frac{%d}{%d}',
-                           $a->{num} * $fa,
-                           $a->{denum} * $fa,
+                           $a->num * $fa,
+                           $a->denum * $fa,
                            $op,
-                           $b->{num} * $fb,
-                           $b->{denum} * $fb
+                           $b->num * $fb,
+                           $b->denum * $fb
                          )
                 );
             push(
                   @way,
                   sprintf(
                            '\frac{%d %s %d}{%d}',
-                           $a->{num} * $fa,
+                           $a->num * $fa,
                            $op,
-                           $b->{num} * $fb,
-                           $a->{denum} * $fa
+                           $b->num * $fb,
+                           $a->denum * $fa
                          )
                 );
-            my $s = $i ? $a->{num} * $fa - $b->{num} * $fb : $a->{num} * $fa + $b->{num} * $fb;
-            push( @way, sprintf( '\frac{%d}{%d}', $s, $a->{denum} * $fa ) );
-            my @c = _reduce( $s, $a->{denum} * $fa );
-            $c[0] != $s and push( @way, sprintf( '\frac{%d}{%d}', @c ) );
+            my $s = VulFrac->new(
+                          num => $i ? $a->num * $fa - $b->num * $fb : $a->num * $fa + $b->num * $fb,
+                          denum => $a->denum );
+            push( @way, "" . $s );
+            my $c = $s->_reduce;
+            $c != $s and push @way, "" . $c;
 
             my $n;
-            $c[0] > $c[1]
+            $c->num > $c->denum
               and push(
                         @way,
                         sprintf(
                                  '\normalsize{%d} \frac{%d}{%d}',
-                                 $n = int( $c[0] / $c[1] ),
-                                 $c[0] - $c[1] * $n, $c[1]
+                                 $n = int($c),
+                                 $c->num - $c->denum * $n,
+                                 $c->denum
                                )
                       );
 
--- lib/App/Math/Trainer/Role/VulFrac.pm
+++ lib/App/Math/Trainer/Role/VulFrac.pm
@@ -14,4 +14,99 @@ use MooX::Options;
 
 our $VERSION = '0.003';
 
+{
+    package    #
+      VulFrac;
+
+    use Moo;
+    use overload
+      '""'   => \&_stringify,
+      '0+'   => \&_numify,
+      'bool' => sub { 1 },
+      '<=>'  => \&_num_compare;
+
+    use Carp qw/croak/;
+    use Scalar::Util qw/blessed/;
+
+    has num => (
+                 is       => "ro",
+                 required => 1
+               );
+
+    has denum => (
+                   is       => "ro",
+                   required => 1
+                 );
+
+    sub _stringify { "\\frac{" . $_[0]->num . "}{" . $_[0]->denum . "}" }
+
+    sub _numify
+    {
+        my $rc = eval sprintf( "(%s)/(%s)", $_[0]->num, $_[0]->denum );
+        $@ and croak $@;
+        return $rc;
+    }
+
+    sub _num_compare
+    {
+        my ( $self, $other, $swapped ) = @_;
+        $swapped and return $other <=> $self->_numify;
+
+        blessed $other or return $self->_numify <=> $other;
+        return $self->_numify <=> $other->_numify;
+    }
+
+    sub _euklid
+    {
+        my ( $a, $b ) = @_;
+        my $h;
+        while ( $b != 0 ) { $h = $a % $b; $a = $b; $b = $h; }
+        return $a;
+    }
+
+    sub _gcd
+    {
+        my ( $a, $b ) = ( $_[0]->num, $_[0]->denum );
+        my $gcd = $a > $b ? _euklid( $a, $b ) : _euklid( $b, $a );
+        return $gcd;
+    }
+
+    sub _reduce
+    {
+        my ( $a, $b ) = ( $_[0]->num, $_[0]->denum );
+        my $gcd = $a > $b ? _euklid( $a, $b ) : _euklid( $b, $a );
+        return
+          VulFrac->new( num   => $_[0]->num / $gcd,
+                        denum => $_[0]->denum / $gcd );
+    }
+
+}
+
+sub _check_vulgar_fraction { my $vf = shift; $vf->num >= 2 and $vf->denum >= 2 }
+
+requires "format";
+
+sub get_vulgar_fractions
+{
+    my ( $self, $amount ) = @_;
+
+    my @result;
+    my ( $max_num, $max_denum ) = @{ $self->format };
+
+    while ( $amount-- )
+    {
+        my $vf;
+        do
+        {
+            my ( $num, $denum ) = ( int( rand($max_num) ), int( rand($max_denum) ) );
+            $vf = VulFrac->new( num   => $num,
+                                denum => $denum );
+        } while ( !_check_vulgar_fraction($vf) );
+
+        push @result, $vf;
+    }
+
+    return @result;
+}
+
 1;
