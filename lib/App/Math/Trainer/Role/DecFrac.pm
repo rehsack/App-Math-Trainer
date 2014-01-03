51e1a9c45def6c358eb5ed3c1fbeacc75a886b77
Jens Rehsack <sno@netbsd.org>
2014-01-03 11:58:43 +0100

    add license/copyright notice where missing

--- lib/App/Math/Trainer/Role/DecFrac.pm
+++ lib/App/Math/Trainer/Role/DecFrac.pm
@@ -30,4 +30,16 @@ sub _check_decimal_fraction
              and length($s1) >= 3 );
 }
 
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010-2014 Jens Rehsack.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
 1;
--- lib/App/Math/Trainer/Role/Unit.pm
+++ lib/App/Math/Trainer/Role/Unit.pm
@@ -338,4 +338,16 @@ sub get_unit_numbers
     return @result;
 }
 
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010-2014 Jens Rehsack.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
 1;
